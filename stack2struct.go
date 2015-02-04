5a4c81cc46719716c94fcf2ef39f475c051d0112
JaycobA <jakob.anders@kaeuferportal.de>
2015-02-04 16:45:54 +0100

    init

---
+++ stack2struct.go
@@ -0,0 +1,64 @@
+package stack2struct
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type stackTrace interface {
+	AddEntry(lineNumber int, packageName string, fileName string, methodName string)
+}
+
+func Parse(trace []byte, stackTrace stackTrace) {
+
+	lines := strings.Split(string(trace), "\n")
+
+	var lineNumber int
+	var fileName, packageName, methodName string
+
+	for index, line := range lines[1:] {
+		if len(line) == 0 {
+			continue
+		}
+		if index%2 == 0 {
+			packageName, methodName = extractPackageName(line)
+		} else {
+			lineNumber, fileName = extractLineNumberAndFile(line)
+			stackTrace.AddEntry(lineNumber, packageName, fileName, methodName)
+		}
+	}
+}
+
+func extractPackageName(line string) (packageName, methodName string) {
+	packagePath, packageNameAndFunction := splitAtLastSlash(line)
+	parts := strings.Split(packageNameAndFunction, ".")
+	packageName = fmt.Sprintf("%s/%s", packagePath, parts[0])
+	methodName = parts[1]
+	return
+}
+
+func extractLineNumberAndFile(line string) (lineNumber int, fileName string) {
+	_, fileAndLine := splitAtLastSlash(line)
+	fileAndLine = removeSpaceAndSuffix(fileAndLine)
+	parts := strings.Split(fileAndLine, ":")
+
+	numberAsString := parts[1]
+	number, _ := strconv.ParseUint(numberAsString, 10, 32)
+	lineNumber = int(number)
+
+	fileName = parts[0]
+	return lineNumber, fileName
+}
+
+func splitAtLastSlash(line string) (left, right string) {
+	parts := strings.Split(line, "/")
+	right = parts[len(parts)-1]
+	left = strings.Join(parts[:len(parts)-1], "/")
+	return
+}
+
+func removeSpaceAndSuffix(line string) string {
+	parts := strings.Split(line, " ")
+	return strings.Join(parts[:len(parts)-1], " ")
+}
---
+++ stack2struct_test.go
@@ -0,0 +1,57 @@
+package stack2struct
+
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	"io/ioutil"
+	"testing"
+)
+
+type testElement struct {
+	lineNumber  int
+	packageName string
+	fileName    string
+	methodName  string
+}
+
+type testStack []*testElement
+
+func (t *testStack) AddEntry(lineNumber int, packageName, fileName, methodName string) {
+	*t = append(*t, &testElement{lineNumber, packageName, fileName, methodName})
+}
+
+func TestStack2Struct(t *testing.T) {
+	Convey("#splitAtLastSlash", t, func() {
+		testLine := "foo/bar/baz"
+		left, right := splitAtLastSlash(testLine)
+		So(left, ShouldEqual, "foo/bar")
+		So(right, ShouldEqual, "baz")
+	})
+
+	Convey("#removeSpaceAndSuffix", t, func() {
+		testLine := "foo:bar baz"
+		result := removeSpaceAndSuffix(testLine)
+		So(result, ShouldEqual, "foo:bar")
+	})
+
+	Convey("#Parse", t, func() {
+		buf, _ := ioutil.ReadFile("fixtures/stack_trace")
+
+		stack := make(testStack, 0, 0)
+		Parse(buf, &stack)
+
+		expectedFirstEntry := testElement{13,
+			"codevault.io/go_projects/raygun4go/stack2struct",
+			"stack2struct_test.go",
+			"func·001()"}
+
+		So(len(stack), ShouldEqual, 5)
+		firstEntry := stack[0]
+		So(firstEntry.lineNumber, ShouldEqual, expectedFirstEntry.lineNumber)
+		So(firstEntry.packageName, ShouldEqual, expectedFirstEntry.packageName)
+		So(firstEntry.fileName, ShouldEqual, expectedFirstEntry.fileName)
+		So(firstEntry.methodName, ShouldEqual, expectedFirstEntry.methodName)
+
+		So(*stack[0], ShouldResemble, expectedFirstEntry)
+	})
+
+}
